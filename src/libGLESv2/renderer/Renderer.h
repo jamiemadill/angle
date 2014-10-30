8c6cfb06dc7cbe4a492f48d714371e07bffdb6d3
Jamie Madill <jmadill@chromium.org>
2014-10-30 10:42:42 -0400

    Move BlitFramebuffer logic into RendererD3D.
    BUG=angle:789
    
    Change-Id: Ib3f830ba2f915a5fa4fa552dccc13c24575aa843

--- src/libGLESv2/renderer/Renderer.h
+++ src/libGLESv2/renderer/Renderer.h
@@ -84,7 +84,7 @@ class Renderer
 
     virtual gl::Error sync(bool block) = 0;
 
-    // TODO(jmadill): pass state and essetial params only
+    // Business logic
     virtual gl::Error drawArrays(const gl::Data &data, GLenum mode,
                                  GLint first, GLsizei count, GLsizei instances) = 0;
     virtual gl::Error drawElements(const gl::Data &data, GLenum mode, GLsizei count, GLenum type,
@@ -97,6 +97,11 @@ class Renderer
     virtual gl::Error clearBufferfi(const gl::Data &data, GLenum buffer, GLint drawbuffer, GLfloat depth, GLint stencil) = 0;
     virtual gl::Error readPixels(gl::Framebuffer *framebuffer, GLint x, GLint y, GLsizei width, GLsizei height, GLenum format,
                                  GLenum type, GLuint outputPitch, const gl::PixelPackState &pack, uint8_t *pixels) = 0;
+
+    virtual gl::Error blitFramebuffer(const gl::Data &data,
+                                      GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1,
+                                      GLint dstX0, GLint dstY0, GLint dstX1, GLint dstY1,
+                                      GLbitfield mask, GLenum filter) = 0;
 
     // TODO(jmadill): caps? and virtual for egl::Display
     virtual bool getShareHandleSupport() const = 0;
