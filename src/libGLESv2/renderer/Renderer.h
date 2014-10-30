eb59e8b5372615ecd1075baaeb854bd611782f98
Jamie Madill <jmadill@chromium.org>
2014-10-30 10:42:48 -0400

    Move the Context ReadPixels logic into RendererD3D.
    BUG=angle:789
    
    Change-Id: I0a678f8fe31a3e7ddd9a675a0e3cb0342299b0e3

--- src/libGLESv2/renderer/Renderer.h
+++ src/libGLESv2/renderer/Renderer.h
@@ -95,8 +95,10 @@ class Renderer
     virtual gl::Error clearBufferuiv(const gl::Data &data, GLenum buffer, GLint drawbuffer, const GLuint *values) = 0;
     virtual gl::Error clearBufferiv(const gl::Data &data, GLenum buffer, GLint drawbuffer, const GLint *values) = 0;
     virtual gl::Error clearBufferfi(const gl::Data &data, GLenum buffer, GLint drawbuffer, GLfloat depth, GLint stencil) = 0;
-    virtual gl::Error readPixels(gl::Framebuffer *framebuffer, GLint x, GLint y, GLsizei width, GLsizei height, GLenum format,
-                                 GLenum type, GLuint outputPitch, const gl::PixelPackState &pack, uint8_t *pixels) = 0;
+
+    virtual gl::Error readPixels(const gl::Data &data, GLint x, GLint y, GLsizei width, GLsizei height,
+                                 GLenum format, GLenum type, GLsizei *bufSize, void* pixels) = 0;
+
 
     virtual gl::Error blitFramebuffer(const gl::Data &data,
                                       GLint srcX0, GLint srcY0, GLint srcX1, GLint srcY1,
